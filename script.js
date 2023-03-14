403d23dcb3079a4f09138a38d4879ee770b11a21
korenmanutd <korenmanutd@gmail.com>
2023-03-14 20:38:43 +1100

    create function that takes playerChoice and computerChoice(random) and return a result

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let computerChoice;
+let computerSelection;
+let result;
 
 // function that chooses random pick
 function getComputerChoice() {
@@ -6,14 +7,46 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
 
     if (rand == 1) {
-        computerChoice = 'Rock';
+        computerSelection = 'Rock';
     } else if (rand == 2) {
-        computerChoice = 'Paper';
+        computerSelection = 'Paper';
     } else {
-        computerChoice = 'Scissors';
+        computerSelection = 'Scissors';
     }
 }
 
-getComputerChoice();
+getComputerChoice()
 
-console.log(computerChoice);
+//function that plays a single game with two parameters (playerSelection, computerSelection)
+
+function playRound(playerSelection, computerSelection) {
+
+    // not to take into account case-sensitivity
+    let player = playerSelection.toLowerCase();
+    let robot = computerSelection.toLowerCase();
+
+    if (player == 'rock' && robot == 'rock') {
+        result = 'Draw! rock and rock!';
+    } else if (player == 'rock' && robot == 'paper'){
+        result = 'You lose! Paper beats Rock!'
+    } else if (player == 'rock' && robot == 'scissors') {
+        result = 'You win! Rock beats Scissors!'
+    } else if (player == 'paper' && robot == 'rock'){
+        result = 'You win! Paper Beats Rock!'
+    } else if (player == 'paper' && robot == 'paper'){
+        result = 'Draw! paper and paper!'
+    } else if (player == 'paper' && robot == 'scissors') {
+        result = 'You lose! Scissors beat Paper!'
+    } else if (player == 'scissors' && robot == 'scissors') {
+        result = 'Draw! Scissors and Scissors!'
+    } else if (player == 'scissors' && robot == 'paper') {
+        result = 'You win! Scissors beat Paper!'
+    } else if (player == 'scissors' && robot == 'rock') {
+        result = 'You lose! Rock beats Scissors!'
+    }
+    return result;
+}
+
+let playerSelection = 'rock';
+console.log(computerSelection)
+console.log(playRound(playerSelection, computerSelection));
